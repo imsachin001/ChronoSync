00c7fb9cfde57cc87d23dfc8e4c85a98e1c4f4e1
Sachin <sachinjangid1016@gmail.com>
2026-03-20 01:16:32 +0530

    mongo url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Add your Clerk Publishable Key to the .env file ya')
 }
 
 
